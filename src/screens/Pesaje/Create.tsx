9b93290642a381f8973d45a810281b545e5a41a4
Anthony Will Solsol Soplin <asolsol@infocasas.com.uy>
2023-10-17 15:32:31 -0500

    as

--- src/screens/Pesaje/Create.tsx
+++ src/screens/Pesaje/Create.tsx
@@ -1,7 +1,26 @@
 import { ScrollView } from "react-native"
 import { Appbar } from "react-native-paper"
+import RNBluetoothClassic from 'react-native-bluetooth-classic';
+import { useEffect, useState } from "react";
 
 const Create = ({navigation}: any) => {
+    const [connectedList ,setConnectedList] = useState<any>();
+    useEffect(() => {
+        getConnectedDevices()
+    }, [])
+
+    const getConnectedDevices = async () => {
+        try {
+            const connected = await RNBluetoothClassic.getConnectedDevices();
+            console.log('connected', connected)
+            setConnectedList(connected);
+        } catch (err) {
+            // Error if Bluetooth is not enabled
+            // Or there are any issues requesting paired devices
+            console.log("ERORRRRR", err);
+        }
+    }
+
     return (
         <ScrollView>
             <Appbar.Header>
